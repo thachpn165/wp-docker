1152d2676523719158999f470c14bcfc78a0740d
Thach Pham Ngoc <thachpn165@gmail.com>
2025-04-09 00:23:35 +0700

    Add language management tests and load language labels setup

---
+++ tests/helpers/load_lang_labels.bash
@@ -0,0 +1,17 @@
+# tests/helpers/load_lang_labels.bash
+
+setup() {
+  export LABEL_LANG_EN="Tiếng Anh"
+  export LABEL_LANG_VI="Tiếng Việt"
+  export LABEL_LANG_FR="Tiếng Pháp"
+  export LABEL_LANG_JA="Tiếng Nhật"
+  export LABEL_LANG_ZH="Tiếng Trung"
+  export LABEL_LANG_KO="Tiếng Hàn"
+  export LABEL_LANG_ES="Tiếng Tây Ban Nha"
+  export LABEL_LANG_DE="Tiếng Đức"
+  export LABEL_LANG_RU="Tiếng Nga"
+  export LABEL_LANG_ID="Tiếng Indonesia"
+  export LABEL_LANG_TH="Tiếng Thái"
+  export LABEL_LANG_MY="Tiếng Malaysia"
+  export LABEL_LANG_TW="Tiếng Đài Loan"
+}
