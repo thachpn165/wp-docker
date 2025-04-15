35360eb9cd464d4da2999ab3f71ef1d94cfa9815
Thach Pham Ngoc <thachpn165@gmail.com>
2025-04-15 17:55:34 +0700

    Fix script to load PHP version logic from the correct file

--- src/shared/scripts/cli/php_get_version.sh
+++ src/shared/scripts/cli/php_get_version.sh
@@ -9,13 +9,15 @@ SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]:-$0}")"
 while [[ "$SCRIPT_PATH" != "/" ]]; do
   if [[ -f "$SCRIPT_PATH/shared/config/load_config.sh" ]]; then
     source "$SCRIPT_PATH/shared/config/load_config.sh"
+
+    load_config_file
     break
   fi
   SCRIPT_PATH="$(dirname "$SCRIPT_PATH")"
 done
 
 # === Load PHP-related logic ===
-safe_source "$FUNCTIONS_DIR/php_loader.sh"
+safe_source "$FUNCTIONS_DIR/php/php_get_version.sh"
 
 # === Execute main logic ===
 php_get_version
