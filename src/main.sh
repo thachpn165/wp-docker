021cc4fc804517540ec853afa9a4d5785bc4539b
Thach Pham Ngoc <thachpn165@gmail.com>
2025-04-23 10:56:28 +0700

    Refactor alias setup process and improve configuration loading in scripts

--- src/main.sh
+++ src/main.sh
@@ -17,6 +17,7 @@ SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]:-$0}")"
 while [[ "$SCRIPT_PATH" != "/" ]]; do
   if [[ -f "$SCRIPT_PATH/shared/config/load_config.sh" ]]; then
     source "$SCRIPT_PATH/shared/config/load_config.sh"
+    load_config_file
     break
   fi
   SCRIPT_PATH="$(dirname "$SCRIPT_PATH")"
